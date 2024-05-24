74ed6c7e6df4b49c0a31deaf4770f3a0cc767a97
htrebor20 <htrebor20@gmail.com>
2024-05-23 21:17:05 -0500

    get capabilities

--- src/app/components/organisms/capability/capability.component.ts
+++ src/app/components/organisms/capability/capability.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CapabilityService } from 'src/app/services/capability/capability.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
-import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
+import { ICapability } from 'src/utils/interfaces/capability/icapability';
+import { IGenericResponse, IPage } from 'src/utils/interfaces/http/httpInterfaces';
 
 @Component({
   selector: 'app-capability',
@@ -9,12 +10,39 @@ import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
   styleUrls: ['./capability.component.scss']
 })
 export class CapabilityComponent implements OnInit {
+  capabilitiesList: ICapability[] = [];
+  totalPages: number = 0;
+  isLoading: boolean = false;
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  order: string = "";
+  requestError: boolean = false
   modalIsOpen: boolean = false;
+  firstLoad: boolean = true;
   response?: IGenericResponse;
 
-  constructor(private capabilityService: CapabilityService, private serviceLoader: LoaderService) {}
+  constructor(private capabilityService: CapabilityService, private serviceLoader: LoaderService) {
+    this.serviceLoader.isLoading$.subscribe(isLoading => {
+      this.isLoading = isLoading;
+    });
+  }
 
   ngOnInit(): void {
+    this.loadCapabilities()
+  }
+
+  loadCapabilities(): void {
+    this.capabilityService.getCapability(this.pageNumber - 1, this.pageSize, this.order).subscribe({
+      next: (response: IPage<ICapability>) => {
+        this.capabilitiesList = response.content;
+        this.totalPages = response.totalPages;
+        this.firstLoad = false;
+      },
+      error: (error: any) => {
+        this.requestError = true
+        this.firstLoad = false;
+      }
+    });
   }
 
   openModal() {
@@ -30,7 +58,25 @@ export class CapabilityComponent implements OnInit {
   }
 
   handleResponse(responseData: IGenericResponse) {
+    this.loadCapabilities()
     this.closeModal();
     this.response = responseData;
   }
+
+  orderChanged(order: string): void {
+    this.order = order;
+    this.loadCapabilities()
+  }
+
+  sizeChanged(size: number): void {
+    this.pageSize = size;
+    this.pageNumber = 1;
+    this.loadCapabilities()
+  }
+
+  pageChanged(page: number) {
+    this.pageNumber = page;
+    this.loadCapabilities()
+  }
+
 }
--- src/app/services/capability/capability.service.ts
+++ src/app/services/capability/capability.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ICapability } from 'src/utils/interfaces/capability/icapability';
-import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
+import { IGenericResponse, IPage } from 'src/utils/interfaces/http/httpInterfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,19 @@ export class CapabilityService {
 
   createCapability(formData: ICapability): Observable<HttpResponse<IGenericResponse>> {
     return this.httpClient.post<IGenericResponse>(this.url, formData, {observe: 'response'} );
+  }
+
+  getCapability(pageNumber: number, pageSize: number, order: string): Observable<IPage<ICapability>> {
+
+    let params = new HttpParams()
+      .set('page', pageNumber.toString())
+      .set('size', pageSize.toString())
+
+    if (order) {
+      params = params.set('sort', order);
+      params = params.set('sortBy', "NAME");
+    }
+
+    return this.httpClient.get<IPage<ICapability>>(this.url, { params });
   }
 }

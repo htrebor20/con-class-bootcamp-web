85981e262a86b5b708ff0e6ddef4b3e634458239
htrebor20 <htrebor20@gmail.com>
2024-05-25 13:05:25 -0500

    feat: add multiple select

--- src/app/components/organisms/capability/capability.component.ts
+++ src/app/components/organisms/capability/capability.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CapabilityService } from 'src/app/services/capability/capability.service';
-import { LoaderService } from 'src/app/services/loader/loader.service';
-import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
+import { TechnologyService } from 'src/app/services/technology/technology.service';
+import { ICapability } from 'src/utils/interfaces/capability/icapability';
+import { ISelectItem } from 'src/utils/interfaces/genericInterfaces';
+import { IGenericResponse, IPage } from 'src/utils/interfaces/http/httpInterfaces';
+import { ITechnology } from 'src/utils/interfaces/technology/interfaces';
+import { createSelectList } from 'src/utils/utils';
 
 @Component({
   selector: 'app-capability',
@@ -12,9 +16,22 @@ export class CapabilityComponent implements OnInit {
   modalIsOpen: boolean = false;
   response?: IGenericResponse;
 
-  constructor(private capabilityService: CapabilityService, private serviceLoader: LoaderService) {}
+  selectedItems: ISelectItem[] = []
+  technologiesList: ISelectItem[] = [];
+
+  constructor(private capabilityService: CapabilityService, private technologyService: TechnologyService) {}
 
   ngOnInit(): void {
+    this.loadTechnologies()
+  }
+
+  loadTechnologies(): void {
+    this.technologyService.getTechnologies(0, 1000, "ASC").subscribe({
+      next: (response: IPage<ITechnology>) => {
+        this.technologiesList = createSelectList(response.content);
+      },
+      error: (error: any) => {}
+    });
   }
 
   openModal() {

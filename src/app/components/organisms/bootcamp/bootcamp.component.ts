178c915e1a6437580123ae4c60c3517b3aece691
htrebor20 <htrebor20@gmail.com>
2024-05-25 13:43:43 -0500

    feat: get bootcamp

--- src/app/components/organisms/bootcamp/bootcamp.component.ts
+++ src/app/components/organisms/bootcamp/bootcamp.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BootcampService } from 'src/app/services/bootcamp/bootcamp.service';
 import { LoaderService } from 'src/app/services/loader/loader.service';
-import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
+import { IBootcamp } from 'src/utils/interfaces/bootcamp/ibootcamp';
+import { IGenericResponse, IPage } from 'src/utils/interfaces/http/httpInterfaces';
 
 @Component({
   selector: 'app-bootcamp',
@@ -9,15 +10,41 @@ import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
   styleUrls: ['./bootcamp.component.scss']
 })
 export class BootcampComponent implements OnInit {
-
+  bootcampList: IBootcamp[] = [];
+  totalPages: number = NaN;
+  isLoading: boolean = false;
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  order: string = "";
+  requestError: boolean = false
   modalIsOpen: boolean = false;
+  firstLoad: boolean = true;
   response?: IGenericResponse;
-
-  constructor(private bootcampService: BootcampService, private serviceLoader: LoaderService) { }
+ 
+  constructor(private bootcampService: BootcampService, private serviceLoader: LoaderService) {
+    this.serviceLoader.isLoading$.subscribe(isLoading => {
+      this.isLoading = isLoading;
+    });
+  }
 
   ngOnInit(): void {
+    this.loadBootcamp()
   }
-  
+
+  loadBootcamp(): void {
+    this.bootcampService.getBootcamp(this.pageNumber - 1, this.pageSize, this.order).subscribe({
+      next: (response: IPage<IBootcamp>) => {
+        this.bootcampList = response.content;
+        this.totalPages = response.totalPages;
+        this.firstLoad = false;
+      },
+      error: (error: any) => {
+        this.requestError = true
+        this.firstLoad = false;
+      }
+    });
+  }
+
   openModal() {
     this.modalIsOpen = true;
   }
@@ -31,7 +58,24 @@ export class BootcampComponent implements OnInit {
   }
 
   handleResponse(responseData: IGenericResponse) {
+    this.loadBootcamp()
     this.closeModal();
     this.response = responseData;
+  }
+
+  orderChanged(order: string): void {
+    this.order = order;
+    this.loadBootcamp()
+  }
+
+  sizeChanged(size: number): void {
+    this.pageSize = size;
+    this.pageNumber = 1;
+    this.loadBootcamp()
+  }
+
+  pageChanged(page: number) {
+    this.pageNumber = page;
+    this.loadBootcamp()
   }
 }
--- src/app/services/bootcamp/bootcamp.service.ts
+++ src/app/services/bootcamp/bootcamp.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IBootcamp } from 'src/utils/interfaces/bootcamp/ibootcamp';
-import { IGenericResponse } from 'src/utils/interfaces/http/httpInterfaces';
+import { IGenericResponse, IPage } from 'src/utils/interfaces/http/httpInterfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +16,19 @@ export class BootcampService {
   createBootcamp(formData: IBootcamp): Observable<HttpResponse<IGenericResponse>> {
     return this.httpClient.post<IGenericResponse>(this.url, formData, {observe: 'response'} );
   }
+
+  getBootcamp(pageNumber: number, pageSize: number, order: string): Observable<IPage<IBootcamp>> {
+
+    let params = new HttpParams()
+      .set('page', pageNumber.toString())
+      .set('size', pageSize.toString())
+
+    if (order) {
+      params = params.set('sort', order);
+      params = params.set('sortBy', "NAME");
+    }
+
+    return this.httpClient.get<IPage<IBootcamp>>(this.url, { params });
+  }
 }
+

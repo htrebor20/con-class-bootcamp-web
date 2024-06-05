8d346943b680c87a99716b80c18d7061eb02b320
htrebor20 <htrebor20@gmail.com>
2024-06-04 19:48:08 -0500

    feat:add headers

--- src/app/services/interceptor/custom.interceptor.ts
+++ src/app/services/interceptor/custom.interceptor.ts
@@ -3,7 +3,8 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpHeaders
 } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,22 +13,20 @@ import { LoaderService } from '../loader/loader.service';
 @Injectable()
 export class CustomInterceptor implements HttpInterceptor {
 
-  private headers = {
-    'Content-Type': 'application/json',
-    'Authorization': environment.token
-  };
+  private headers = new HttpHeaders()
+    .set('Content-Type', 'application/json')
+    .set('Authorization', environment.token)
 
-  constructor(private readonly serviceLoader: LoaderService ) {}
+  constructor(private readonly serviceLoader: LoaderService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.serviceLoader.show();
-    
     const clonedRequest = request.clone({
-      setHeaders: this.headers
+      headers: this.headers
     });
-    
+
     return next.handle(clonedRequest).pipe(
-      finalize(()=>this.serviceLoader.hide())
+      finalize(() => this.serviceLoader.hide())
     );
   }
 }
